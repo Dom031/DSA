0a623c7badbf6e6753a27433928e3ab5bedc5b1d
Dom <142145085+Dom031@users.noreply.github.com>
2025-06-22 23:02:02 +0100

    Time: 0 ms (100.00%) | Memory: 13.4 MB (61.82%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,21 +1,19 @@
 class Solution(object):
     def search(self, nums, target):
-        #Dom
         """
         :type nums: List[int]
         :type target: int
         :rtype: int
         """
-        first = 0
-        last = len(nums) -1
+        l, r = 0, len(nums)-1 
 
-        while first <= last:
-            mid = (first + last) // 2
+        while l <= r:
+            mid = (l + r) // 2
             if nums[mid] == target:
-                return nums.index(target)
+                return mid
             elif nums[mid] < target:
-                first = mid + 1
+                 l = mid + 1
             else:
-                last = mid - 1
-        return -1
+                r = mid - 1
 
+        return -1
